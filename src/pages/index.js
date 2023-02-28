f898cab034978ba14031bbe162096bbe2c6671ad
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2023-02-28 11:50:46 +0700

    redo

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            Elza Natali Wijaya | 460542 
+            [Nama] | [NIU] 
           </h5>
         </div>
       </main>
